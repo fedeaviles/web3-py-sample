ff665a01530ecbc6e208fe9e0fb0633a4a2a65ba
Federico Aviles <federico.aviles@ripio.com>
2021-12-20 09:44:09 -0300

    refactor functions

--- functions.py
+++ functions.py
@@ -1,43 +1,37 @@
 from settings import w3, contract, first_block
 
-def create_product(name, address, private_key):
-    tx = contract.functions.createProduct(name).buildTransaction(
+
+def send_transaction(transaction, address, private_key):
+    tx = transaction.buildTransaction(
         {
-            'from': address,
-            'nonce': w3.eth.getTransactionCount(address),
-            'gas': 2000000,
-            'gasPrice': w3.toWei('40', 'gwei')
+            "from": address,
+            "nonce": w3.eth.get_transaction_count(address),
+            "gas": 2000000,
+            "gasPrice": w3.toWei("40", "gwei"),
         }
     )
     signed_tx = w3.eth.account.sign_transaction(tx, private_key)
     return w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+
+
+def create_product(name, address, private_key):
+    transaction = contract.functions.createProduct(name)
+    return send_transaction(transaction, address, private_key)
+
 
 def delegate_product(product_id, from_address, from_private_key, to_address):
-    tx = contract.functions.delegateProduct(product_id, to_address).buildTransaction(
-        {
-            'from': from_address,
-            'nonce': w3.eth.getTransactionCount(from_address),
-            'gas': 2000000,
-            'gasPrice': w3.toWei('40', 'gwei')
-        }
-    )
-    signed_tx = w3.eth.account.sign_transaction(tx, from_private_key)
-    return w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    transaction = contract.functions.delegateProduct(product_id, to_address)
+    return send_transaction(transaction, from_address, from_private_key)
+
 
 def accept_product(product_id, address, private_key):
-    tx = contract.functions.acceptProduct(product_id).buildTransaction(
-        {
-            'from': address,
-            'nonce': w3.eth.getTransactionCount(address),
-            'gas': 2000000,
-            'gasPrice': w3.toWei('40', 'gwei')
-        }
-    )
-    signed_tx = w3.eth.account.sign_transaction(tx, private_key)
-    return w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    transaction = contract.functions.acceptProduct(product_id)
+    return send_transaction(transaction, address, private_key)
+
 
 def get_product(product_id):
     return contract.functions.products(product_id).call()
+
 
 def get_all_products():
     products_count = contract.functions.size().call()
@@ -46,22 +40,34 @@ def get_all_products():
         all_products.append(get_product(i))
     return all_products
 
+
 def get_product_by_name(name):
-    new_product_event_filter = contract.events.NewProduct.createFilter(fromBlock=first_block, argument_filters={'name': name})
+    new_product_event_filter = contract.events.NewProduct.createFilter(
+        fromBlock=first_block, argument_filters={"name": name}
+    )
     all_products = new_product_event_filter.get_all_entries()
     return all_products
 
+
 def get_delegated_products():
-    delegated_product_event_filter = contract.events.DelegateProduct.createFilter(fromBlock=first_block)
+    delegated_product_event_filter = contract.events.DelegateProduct.createFilter(
+        fromBlock=first_block
+    )
     all_products = delegated_product_event_filter.get_all_entries()
     return all_products
+
 
 def get_delegated_products_by_owner(new_owner):
-    delegated_product_event_filter = contract.events.DelegateProduct.createFilter(fromBlock=first_block, argument_filters={'newOwner': new_owner})
+    delegated_product_event_filter = contract.events.DelegateProduct.createFilter(
+        fromBlock=first_block, argument_filters={"newOwner": new_owner}
+    )
     all_products = delegated_product_event_filter.get_all_entries()
     return all_products
 
+
 def get_accepted_products():
-    accepted_product_event_filter = contract.events.AcceptProduct.createFilter(fromBlock=first_block)
+    accepted_product_event_filter = contract.events.AcceptProduct.createFilter(
+        fromBlock=first_block
+    )
     all_products = accepted_product_event_filter.get_all_entries()
     return all_products
