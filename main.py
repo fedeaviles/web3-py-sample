d45d5bb2187f30c924ab60a305106bf501ab7120
Federico Aviles <federico.aviles@ripio.com>
2021-12-22 09:07:31 -0300

    adding endpoints

---
+++ main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import NonNegativeInt, BaseModel
+from app.exceptions import InvalidProductId
+import app.functions as functions
+
+app = FastAPI()
+
+
+class ProductCreate(BaseModel):
+    name: str
+    address: str
+    key: str
+
+
+class ProductDelegate(BaseModel):
+    address: str
+    key: str
+    new_address: str
+
+
+class ProductAccept(BaseModel):
+    address: str
+    key: str
+
+
+@app.get("/products")
+async def read_products():
+    return functions.get_all_products()
+
+
+@app.get("/products/{id}")
+async def read_product(id: NonNegativeInt):
+    try:
+        return functions.get_product(id)
+    except (InvalidProductId):
+        raise HTTPException(status_code=404, detail="Product not found")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.post("/products")
+async def create_product(product: ProductCreate):
+    try:
+        tx = functions.create_product(product.name, product.address, product.key)
+        return {"transaction_hash": tx.hex()}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.post("/products/{id}/delegate")
+async def delegate_product(id: int, product: ProductDelegate):
+    try:
+        tx = functions.delegate_product(
+            id, product.address, product.key, product.new_address
+        )
+        return {"transaction_hash": tx.hex()}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.post("/products/{id}/accept")
+async def accept_product(id: int, product: ProductAccept):
+    try:
+        tx = functions.accept_product(id, product.address, product.key)
+        return {"transaction_hash": tx.hex()}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
