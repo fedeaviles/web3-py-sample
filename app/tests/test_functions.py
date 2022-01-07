e906430f156db1dc6bf4461568c6b84f9dcd1cdf
Federico Aviles <federico.aviles@ripio.com>
2022-01-07 14:58:17 -0300

    adding tests

--- app/tests/test_functions.py
+++ app/tests/test_functions.py
@@ -32,6 +32,58 @@ def test_get_product_raise_exception(mock_contract, mock_w3):
         get_product(0)
 
 
+def test_get_filtered_product_by_name(mock_contract, mock_w3, account_1):
+    create_product("test_product0", account_1.address, account_1.key)
+    create_product("test_product1", account_1.address, account_1.key)
+
+    products = get_filtered_products(name="test_product0")
+
+    assert len(products) == 1
+    assert products[0]["name"] == "test_product0"
+
+
+def test_get_filtered_product_by_owner(mock_contract, mock_w3, account_1, account_2):
+    create_product("test_product0", account_1.address, account_1.key)
+    create_product("test_product1", account_1.address, account_1.key)
+    create_product("test_product2", account_2.address, account_2.key)
+
+    products = get_filtered_products(owner=account_1.address)
+
+    for i in range(0, len(products)):
+        assert products[i]["owner"] == account_1.address
+
+    assert len(products) == 2
+
+
+def test_get_filtered_product_by_status(mock_contract, mock_w3, account_1, account_2):
+    create_product("test_product0", account_1.address, account_1.key)
+    create_product("test_product1", account_1.address, account_1.key)
+
+    delegate_product(0, account_1.address, account_1.key, account_2.address)
+
+    products = get_filtered_products(status=0)
+
+    assert products[0]["id"] == 1
+    assert products[0]["status"] == 0
+
+    assert len(products) == 1
+
+
+def test_get_filtered_product_by_newOwner(mock_contract, mock_w3, account_1, account_2):
+    create_product("test_product0", account_1.address, account_1.key)
+    create_product("test_product1", account_1.address, account_1.key)
+    create_product("test_product2", account_2.address, account_2.key)
+
+    delegate_product(0, account_1.address, account_1.key, account_2.address)
+
+    products = get_filtered_products(newOwner=account_2.address)
+
+    for i in range(0, len(products)):
+        assert products[i]["newOwner"] == account_2.address
+
+    assert len(products) == 1
+
+
 def test_account_cannot_create_more_than_eleven_products(
     mock_contract, mock_w3, account_1
 ):
@@ -92,3 +144,37 @@ def test_get_all_products(mock_contract, mock_w3, account_1):
         assert products[i]["newOwner"] == "0x0000000000000000000000000000000000000000"
 
     assert len(products) == 2
+
+
+def test_get_delegated_products(
+    mock_contract, mock_w3, mock_first_block, account_1, account_2
+):
+    create_product("test_product0", account_1.address, account_1.key)
+    create_product("test_product1", account_1.address, account_1.key)
+
+    delegate_product(0, account_1.address, account_1.key, account_2.address)
+
+    delegated_products = get_delegated_products()
+
+    assert len(delegated_products) == 1
+
+    assert delegated_products[0]["args"]["productId"] == 0
+    assert delegated_products[0]["args"]["newOwner"] == account_2.address
+    assert delegated_products[0]["args"]["status"] == 1
+
+
+def test_get_accepted_products(
+    mock_contract, mock_w3, mock_first_block, account_1, account_2
+):
+    create_product("test_product0", account_1.address, account_1.key)
+    create_product("test_product1", account_1.address, account_1.key)
+
+    delegate_product(0, account_1.address, account_1.key, account_2.address)
+    accept_product(0, account_2.address, account_2.key)
+
+    accepted_products = get_accepted_products()
+
+    assert len(accepted_products) == 1
+
+    assert accepted_products[0]["args"]["productId"] == 0
+    assert accepted_products[0]["args"]["status"] == 0
